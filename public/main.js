d9e4de0d9a22f4ed4e8adebc277543b2b5ee2b83
Dino Ribeiro <DRibeiro1124@gmail.com>
2018-08-01 21:07:37 -0400

    added my first two lines of JS

--- public/main.js
+++ public/main.js
@@ -4,3 +4,9 @@
 
 // document.addEventListener('DOMContentLoaded', main)
 
+const homeTeamButtonClickEvent = () => {
+  console.log('Home team button was clicked')
+  const currentValue = document.querySelector('.home-team-name').value
+}
+
+document.querySelector('.home-team-name-button').addEventListener('click', homeTeamButtonClickEvent)
