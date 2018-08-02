44d3cd96bb1be2202caae5f8d949a6d9153bf0d3
Dino Ribeiro <DRibeiro1124@gmail.com>
2018-08-01 21:37:49 -0400

    JS update team name done

--- public/main.js
+++ public/main.js
@@ -5,8 +5,10 @@
 // document.addEventListener('DOMContentLoaded', main)
 
 const homeTeamButtonClickEvent = () => {
-  console.log('Home team button was clicked')
+  console.log('Home button was clicked')
   const currentValue = document.querySelector('.home-team-name').value
+  console.log(currentValue)
+  document.querySelector('.home-team').textContent = currentValue
 }
 
 document.querySelector('.home-team-name-button').addEventListener('click', homeTeamButtonClickEvent)
