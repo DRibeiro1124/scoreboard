4adcd5c17c475680230a1836043dbde1146eacdf
Dino Ribeiro <DRibeiro1124@gmail.com>
2018-08-01 22:14:31 -0400

    added plus and minus goals scored buttons

--- public/main.js
+++ public/main.js
@@ -5,10 +5,36 @@
 // document.addEventListener('DOMContentLoaded', main)
 
 const homeTeamButtonClickEvent = () => {
-  console.log('Home button was clicked')
+  console.log('Home team update button was clicked')
   const currentValue = document.querySelector('.home-team-name').value
   console.log(currentValue)
   document.querySelector('.home-team').textContent = currentValue
 }
 
+const awayTeamButtonClickEvent = () => {
+  console.log('Away team update button was clicked')
+  const currentValue = document.querySelector('.away-team-name').value
+  console.log(currentValue)
+  document.querySelector('.away-team').textContent = currentValue
+}
+
+const homeAddGoalButtonClickEvent = () => {
+  console.log('Plus one goal')
+  const currentValue = parseInt(document.querySelector('.home-score').textContent)
+  console.log(currentValue)
+  document.querySelector('.home-score').textContent = currentValue + 1
+}
+
+const homeSubtractGoalButtonClickEvent = () => {
+  console.log('Minus one goal')
+  const currentValue = parseInt(document.querySelector('.home-score').textContent)
+  console.log(currentValue)
+  if(currentValue > 0) {
+    document.querySelector('.home-score').textContent = currentValue -1
+  }
+}
+
 document.querySelector('.home-team-name-button').addEventListener('click', homeTeamButtonClickEvent)
+document.querySelector('.away-team-name-button').addEventListener('click', awayTeamButtonClickEvent)
+document.querySelector('.home-add-goal').addEventListener('click', homeAddGoalButtonClickEvent)
+document.querySelector('.home-minus-goal').addEventListener('click', homeSubtractGoalButtonClickEvent)
